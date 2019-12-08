bf1297cdbd2acff6b4fb4bfb752ba5c637651c8f
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2019-12-08 16:37:56 -0700

    Use QImage.rgbSwapped() instead of manually swapping R and B
    I did not know this existed until just now. Nice

--- libimagequant_integrations/_Qt.py
+++ libimagequant_integrations/_Qt.py
@@ -9,18 +9,11 @@ def to_liq(QtGui, image, attr, is_pyqt):
     if image.format() != QtGui.QImage.Format_ARGB32:
         image = image.convertToFormat(QtGui.QImage.Format_ARGB32)
 
-    argb_data = image.bits()
-    if is_pyqt:
-        argb_data = argb_data.asstring(image.byteCount())
+    image = image.rgbSwapped()
 
-    # BGRA -> RGBA
-    def gen_rgba_indices():
-        for i in range(0, len(argb_data), 4):
-            yield i + 2 # r
-            yield i + 1 # g
-            yield i + 0 # b
-            yield i + 3 # a
-    rgba_data = bytes(argb_data[idx] for idx in gen_rgba_indices())
+    rgba_data = image.bits()
+    if is_pyqt:
+        rgba_data = rgba_data.asstring(image.byteCount())
 
     return attr.create_rgba(rgba_data, image.width(), image.height(), 0)
 

dafd8172ec9cac90e8900401e42a5c6aeab3feb9
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2020-10-09 18:43:44 -0400

    Add transparency to test suite

--- test_liq_integrations.py
+++ test_liq_integrations.py
@@ -35,13 +35,20 @@ import libimagequant as liq
 import pytest
 
 
-TEST_IMAGE = 'test_res/gradient.png'
-TEST_POINTS = [
+TEST_IMAGE_1 = 'test_res/gradient.png'
+TEST_POINTS_1 = [
     # ((x,   y), (  r,   g,   b,   a))
     ((  0,   0), (  0,   0, 128, 255)),
     ((511,   0), (255,   0, 139, 255)),
     ((  0, 511), (  0, 255, 117, 255)),
     ((511, 511), (255, 255, 126, 255))]
+TEST_IMAGE_2 = 'test_res/gradient_alpha.png'
+TEST_POINTS_2 = [
+    # ((x,   y), (  r,   g,   b,   a))
+    ((  0,   0), (  0,   0, 128, 254)),
+    ((511,   0), (255,   0, 139, 127)),
+    ((  0, 511), (  0, 255, 117, 127)),
+    ((511, 511), (255, 255, 126, 0))]
 
 
 def near(a, b):
@@ -59,28 +66,34 @@ def test_PIL():
     """
     Test libimagequant_integrations.PIL
     """
-    pil_in = PIL.Image.open(TEST_IMAGE)
+    def test_against(image_filename, points):
+        pil_in = PIL.Image.open(image_filename)
 
-    for pos, expected in TEST_POINTS:
-        assert pil_in.getpixel(pos) == expected
+        for pos, expected in points:
+            assert pil_in.getpixel(pos) == expected
 
-    attr = liq.Attr()
-    img = liq_PIL.to_liq(pil_in, attr)
+        attr = liq.Attr()
+        img = liq_PIL.to_liq(pil_in, attr)
 
-    result = img.quantize(attr)
-    pil_out = liq_PIL.from_liq(result, img)
+        result = img.quantize(attr)
+        pil_out = liq_PIL.from_liq(result, img)
 
-    assert pil_out.mode == 'P'
+        assert pil_out.mode == 'P'
 
-    pil_out_rgba = pil_out.convert('RGBA')
+        pil_out_rgba = pil_out.convert('RGBA')
 
-    for pos, expected in TEST_POINTS:
-        r, g, b, a = pil_out_rgba.getpixel(pos)
-        print(pos, expected, (r, g, b, a))
-        assert near(r, expected[0])
-        assert near(g, expected[1])
-        assert near(b, expected[2])
-        assert a == 255
+        for pos, expected in points:
+            r, g, b, a = pil_out_rgba.getpixel(pos)
+            print(pos, expected, (r, g, b, a))
+            assert near(r, expected[0])
+            assert near(g, expected[1])
+            assert near(b, expected[2])
+            assert near(a, expected[3])
+
+    test_against(TEST_IMAGE_1, TEST_POINTS_1)
+    # PIL doesn't support paletted images with transparency (the "PA"
+    # image mode actually uses separate index and alpha channels for
+    # each pixel), so we don't test against image 2
 
 
 def run_qt_test(liq_Qt, QtGui):
@@ -89,30 +102,34 @@ def run_qt_test(liq_Qt, QtGui):
     """
     app = QtGui.QGuiApplication([])
 
-    q_in = QtGui.QImage(TEST_IMAGE)
+    def test_against(image_filename, points):
+        q_in = QtGui.QImage(image_filename)
 
-    for pos, expected in TEST_POINTS:
-        qcolor = q_in.pixelColor(*pos)
-        assert (qcolor.red(), qcolor.green(), qcolor.blue(), qcolor.alpha()) == expected
+        for pos, expected in points:
+            qcolor = q_in.pixelColor(*pos)
+            assert (qcolor.red(), qcolor.green(), qcolor.blue(), qcolor.alpha()) == expected
 
-    attr = liq.Attr()
-    img = liq_Qt.to_liq(q_in, attr)
+        attr = liq.Attr()
+        img = liq_Qt.to_liq(q_in, attr)
 
-    result = img.quantize(attr)
-    q_out = liq_Qt.from_liq(result, img)
+        result = img.quantize(attr)
+        q_out = liq_Qt.from_liq(result, img)
 
-    assert q_out.format() == QtGui.QImage.Format_Indexed8
+        assert q_out.format() == QtGui.QImage.Format_Indexed8
 
-    q_out_rgba = q_out.convertToFormat(QtGui.QImage.Format_ARGB32)
+        q_out_rgba = q_out.convertToFormat(QtGui.QImage.Format_ARGB32)
 
-    for pos, expected in TEST_POINTS:
-        qcolor = q_out_rgba.pixelColor(*pos)
-        r, g, b, a = qcolor.red(), qcolor.green(), qcolor.blue(), qcolor.alpha()
-        print(pos, expected, (r, g, b, a))
-        assert near(r, expected[0])
-        assert near(g, expected[1])
-        assert near(b, expected[2])
-        assert a == 255
+        for pos, expected in points:
+            qcolor = q_out_rgba.pixelColor(*pos)
+            r, g, b, a = qcolor.red(), qcolor.green(), qcolor.blue(), qcolor.alpha()
+            print(pos, expected, (r, g, b, a))
+            assert near(r, expected[0])
+            assert near(g, expected[1])
+            assert near(b, expected[2])
+            assert near(a, expected[3])
+
+    test_against(TEST_IMAGE_1, TEST_POINTS_1)
+    test_against(TEST_IMAGE_2, TEST_POINTS_2)
 
 
 @pytest.mark.skipif(PyQt5 is None, reason='PyQt5 is not available')
@@ -137,25 +154,29 @@ def test_skimage():
     """
     Test libimagequant_integrations.skimage
     """
-    sk_in = skimage.io.imread(TEST_IMAGE)
-    assert sk_in.shape[2] == 4
+    def test_against(image_filename, points):
+        sk_in = skimage.io.imread(image_filename)
+        assert sk_in.shape[2] == 4
 
-    for (x, y), expected in TEST_POINTS:
-        assert np.array_equal(sk_in[y, x], np.array(expected))
+        for (x, y), expected in points:
+            assert np.array_equal(sk_in[y, x], np.array(expected))
 
-    attr = liq.Attr()
-    img = liq_skimage.to_liq(sk_in, attr)
+        attr = liq.Attr()
+        img = liq_skimage.to_liq(sk_in, attr)
 
-    result = img.quantize(attr)
-    sk_out_px, sk_out_pal = liq_skimage.from_liq(result, img)
+        result = img.quantize(attr)
+        sk_out_px, sk_out_pal = liq_skimage.from_liq(result, img)
 
-    assert sk_out_px.shape[2] == 1
-    assert sk_out_pal.shape[1] == 4
+        assert sk_out_px.shape[2] == 1
+        assert sk_out_pal.shape[1] == 4
 
-    for (x, y), expected in TEST_POINTS:
-        r, g, b, a = sk_out_pal[sk_out_px[y, x][0]]
-        print((x, y), expected, (r, g, b, a))
-        assert near(r, expected[0])
-        assert near(g, expected[1])
-        assert near(b, expected[2])
-        assert a == 255
+        for (x, y), expected in points:
+            r, g, b, a = sk_out_pal[sk_out_px[y, x][0]]
+            print((x, y), expected, (r, g, b, a))
+            assert near(r, expected[0])
+            assert near(g, expected[1])
+            assert near(b, expected[2])
+            assert near(a, expected[3])
+
+    test_against(TEST_IMAGE_1, TEST_POINTS_1)
+    test_against(TEST_IMAGE_2, TEST_POINTS_2)

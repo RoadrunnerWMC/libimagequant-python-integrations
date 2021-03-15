bae3a58d27b3dfcfda99c8a7c632cd6c47e377e8
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2021-03-14 21:51:09 -0400

    Version bump: 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='libimagequant_integrations',
-    version='1.1.0',
+    version='1.1.1',
     author='RoadrunnerWMC',
     author_email='roadrunnerwmc@gmail.com',
     description='',
